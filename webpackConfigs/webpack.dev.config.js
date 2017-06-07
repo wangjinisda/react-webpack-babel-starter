352b661cc607afa509e390ed46eba045a3223826
jin wang <v-jiwag@microsoft.com>
2017-06-07 11:51:25 +0800

    Jiwag (#1)
    * change webpack config
    
    * add node
    
    * node update
    
    * note update
    
    * node update
    
    * node update
    
    * node update
    
    * add filelist plugin to webpack
    
    * add webpack-shell-plugin
    
    * file update
    
    * with style loader
    
    * file update
    
    * replace style loader with
    
    * remive webpack dev config file
    
    * update package.json
    
    * update rules

--- webpackConfigs/webpack.dev.config.js
+++ webpackConfigs/webpack.dev.config.js
@@ -1,7 +1,12 @@
-const webpack         = require("webpack");
-const {resolve}       = require("path");
-const StyleLintPlugin = require('stylelint-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack            = require("webpack");
+const {resolve}          = require("path");
+const StyleLintPlugin    = require('stylelint-webpack-plugin');
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
+
+const FileListPlugin     = require('./plugins/FileListPlugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const rules              = require('./rules/index');
+const ExtractTextPlugin  = require('extract-text-webpack-plugin');
 
 module.exports = {
     resolve: {
@@ -12,7 +17,7 @@ module.exports = {
         vendor: ['react', 'react-dom']
     },
     output:  {
-        filename: '[name].[chunkhash].js',
+        filename: '[name].[hash].js',
         path:       resolve(__dirname, "../public"),
         publicPath: "/" // necessary for HMR to know where to load the hot update chunks
     },
@@ -28,49 +33,34 @@ module.exports = {
 
     module: {
         rules: [
-            {
-                test:    /\.(js|jsx)$/,
-                use:     ["babel-loader"],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use:  ["style-loader", "css-loader?modules", "postcss-loader",],
-            },
-            {
-                test:    /\.scss$/,
-                loaders: ["style-loader", "css-loader?modules", "postcss-loader", "sass-loader"]
-            },
-            {
-                test:    /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
-                    'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-                    'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false'
-                ]
-            },
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            { test: /\.jade$/, loader: 'jade-loader' }
+            rules.imageRules.images,
+            rules.fontRules.fonts,
+            rules.jadeRules.jade,
+            rules.stylesRules.cssExtractTextPlugin,
+            rules.stylesRules.scssExtractTextPlugin,
+            rules.jsRules.reactJs,
+            rules.jsRules.ts,
         ],
     },
 
     plugins:     [
+        new FileListPlugin({options: true}),
         new StyleLintPlugin(),
-        //new webpack.HotModuleReplacementPlugin(), // enable HMR globally
+        new webpack.HotModuleReplacementPlugin(), // enable HMR globally
         new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
-         new HtmlWebpackPlugin({
+        new HtmlWebpackPlugin({
             filename: 'index.html',
             favicon: resolve(__dirname, "../src/jade") +  '/favicon.ico',
             template: resolve(__dirname, "../src/jade") + '/template.jade',
             title: 'Jade demo'
         }),
         new webpack.optimize.CommonsChunkPlugin({
-            name: ['vendor', 'manifest'] // Specify the common bundle's name.
-        })
-
+            name: 'chunks', // Specify the common bundle's name.
+            filename: `chunks-[hash].js`,
+        }),
+        new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildEnd:['echo "Webpack End"']}),
+        // 
+        new ExtractTextPlugin("style.css")
     ],
     performance: {
         hints: false
