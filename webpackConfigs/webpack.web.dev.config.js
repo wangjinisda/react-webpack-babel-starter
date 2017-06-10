4f841d28b405f0441c55f48ffc3cda9b857a882a
Jin Wang (Inspur) <v-jiwag@microsoft.com>
2017-06-10 22:07:07 +0800

    upate server config

--- webpackConfigs/webpack.web.dev.config.js
+++ webpackConfigs/webpack.web.dev.config.js
@@ -47,7 +47,7 @@ module.exports = {
 
     plugins:     [
         new FileListPlugin({options: true}),
-        // new StyleLintPlugin(),
+        new StyleLintPlugin(),
         new webpack.HotModuleReplacementPlugin(), // enable HMR globally
         new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
         new HtmlWebpackPlugin({
