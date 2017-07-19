e1cd82a1f5a508dae4b76822599b4607d0e70fbd
jin wang <v-jiwag@microsoft.com>
2017-07-19 21:39:54 +0800

    scss pack updaye

--- webpackConfigs/others/webpack.scss.dev.config.js
+++ webpackConfigs/others/webpack.scss.dev.config.js
@@ -5,16 +5,17 @@ const rules                = require('./../rules/index');
 const { resolve }          = require("path");
 
 const rootPath             = resolve(process.cwd(), './');
-const styles_LTR           = resolve(rootPath, './src/client/styles/styles-LTR.scss');
+const styles_LTR           = resolve(rootPath, './src/market/mac/styles/styles-LTR.scss');
 
-const styles_RTL           = resolve(rootPath, './src/client/styles/styles-RTL.scss');
+const styles_RTL           = resolve(rootPath, './src/market/mac/styles/styles-RTL.scss');
 
 module.exports = {
     resolve: {
         extensions: [".webpack.js", ".web.js", ".js", ".jsx", ".tsx", ".ts"],
         modules: [
-            resolve(process.cwd(), './src/_shared'),
-            resolve(process.cwd(), './src/_shared/styles/core'),
+            resolve(process.cwd(), './src/resources/external_artifacts/MWF_1.6.0/core'),
+            resolve(process.cwd(), './src/resources/external_artifacts/MWF_1.6.0/core/styles'),
+            resolve(process.cwd(), './src/market/shared'),
             resolve(process.cwd(), './node_modules')
         ],
     },
--- webpackConfigs/rules/stylesRules.js
+++ webpackConfigs/rules/stylesRules.js
@@ -1,102 +1,112 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { resolve } = require("path");
 
 const rules = {
-        // ...
-        css:{
-          test: /\.css$/,
-          use:  ["style-loader", "css-loader?modules", "postcss-loader",],
-        },
-        scss: {
-          test: /\.scss$/,
-          loaders: ["style-loader", "css-loader", "sass-loader"],
-          //exclude: path.resolve(__dirname, 'src/app')
-        },
-        cssExtractTextPlugin:{
-                  test: /\.css$/,
-                  exclude: /node_modules/,
-                  use: ExtractTextPlugin.extract({
-                      fallback: 'style-loader',
+    // ...
+    css: {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader?modules", "postcss-loader",],
+    },
+    scss: {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        //exclude: path.resolve(__dirname, 'src/app')
+    },
+    cssExtractTextPlugin: {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
 
-                      // Could also be write as follow:
-                      // use: 'css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
-                      use: [
-                          {
-                              loader: 'css-loader',
-                              query: {
-                                  modules: true,
-                                  localIdentName: '[name]__[local]___[hash:base64:5]'
-                              }
-                          },
-                          'postcss-loader'
-                      ]
-                  }),
-        },
-        cssExtractTextPluginPureNaming:{
-                  test: /\.css$/,
-                  exclude: /node_modules/,
-                  use: ExtractTextPlugin.extract({
-                      fallback: 'style-loader',
+            // Could also be write as follow:
+            // use: 'css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+            use: [
+                {
+                    loader: 'css-loader',
+                    query: {
+                        modules: true,
+                        localIdentName: '[name]__[local]___[hash:base64:5]'
+                    }
+                },
+                'postcss-loader'
+            ]
+        }),
+    },
+    cssExtractTextPluginPureNaming: {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
 
-                      // Could also be write as follow:
-                      // use: 'css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
-                      use: [
-                          {
-                              loader: 'css-loader',
-                              query: {
-                                  modules: true,
-                                  localIdentName: '[local]'
-                              }
-                          },
-                          'postcss-loader'
-                      ]
-                  }),
-        },
-        scssExtractTextPlugin:{
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
+            // Could also be write as follow:
+            // use: 'css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+            use: [
+                {
+                    loader: 'css-loader',
+                    query: {
+                        modules: true,
+                        localIdentName: '[local]'
+                    }
+                },
+                'postcss-loader'
+            ]
+        }),
+    },
+    scssExtractTextPlugin: {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
 
-                    // Could also be write as follow:
-                    // use: 'css-loader?modules&importLoader=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader'
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            query: {
-                                modules: true,
-                                sourceMap: true,
-                                importLoaders: 2,
-                                localIdentName: '[name]__[local]___[hash:base64:5]'
-                            }
-                        },
-                        'sass-loader'
-                    ]
-                }),
-        },
-        scssExtractTextPluginPureNaming:{
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
+            // Could also be write as follow:
+            // use: 'css-loader?modules&importLoader=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader'
+            use: [
+                {
+                    loader: 'css-loader',
+                    query: {
+                        modules: true,
+                        sourceMap: true,
+                        importLoaders: 2,
+                        localIdentName: '[name]__[local]___[hash:base64:5]'
+                    }
+                },
+                'sass-loader'
+            ]
+        }),
+    },
+    scssExtractTextPluginPureNaming: {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
 
-                    // Could also be write as follow:
-                    // use: 'css-loader?modules&importLoader=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader'
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            query: {
-                                modules: true,
-                                sourceMap: true,
-                                importLoaders: 2,
-                                localIdentName: '[local]'
-                            }
-                        },
-                        'sass-loader'
-                    ]
-                }),
-        }
+            // Could also be write as follow:
+            // use: 'css-loader?modules&importLoader=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader'
+            use: [
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true,
+                        sourceMap: true,
+                        importLoaders: 2,
+                        localIdentName: '[local]',
+                        import: true
+                    }
+                },
+                {
+                    loader: "sass-loader",
+                    options: {
+                        includePaths: [
+                            resolve(__dirname, "./../../src/resources/external_artifacts/MWF_1.6.0"),
+                            resolve(__dirname, "./../../src/resources/external_artifacts/MWF_1.6.0/core/styles")
+                        ]
+                    }
+                }
+            ]
+        }),
+    }
 
-  // ...
+    // ...
 }
 
 module.exports = rules;
